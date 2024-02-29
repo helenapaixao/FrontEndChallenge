d25b96d042d342757c99f653b27dd2cbf59ea3c7
helenapaixao <hp.helenapaixao@gmail.com>
2024-02-29 12:45:12 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
+      <main>
+        <h1 className="font-mono text-xl code">
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  )
+}
