23af9d23c0fbe2f838e7ead6cb98a222bf398110
helenapaixao <hp.helenapaixao@gmail.com>
2024-02-29 13:18:25 -0300

    chore: Update profile details UI
    - Centered the name in the profile section
    - Applied star icons for ratings in the reservation table
    - Added Inter font to the project
    - Adjusted styles for status indicators in the reservation table
    - Integrated FontAwesome icon for ellipsis in the actions column
    - Made various small style improvements and fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar, faEllipsisV } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
@@ -9,7 +9,7 @@ export default function Home() {
           <tr>
             <th
               scope="col"
-              className="px-6 py-4 font-medium  font-inter text-gray-500"
+              className="px-6 py-4 font-medium font-inter text-gray-500"
             >
               CAR
             </th>
@@ -38,15 +38,16 @@ export default function Home() {
                 />
               </div>
               <div className="text-sm flex items-center">
-                <div className="font-medium text-gray-900">Steven Jobs</div>
+                <div className="font-inter font-medium text-gray-900 text-base">
+                  Mini Cooper 2020
+                </div>
               </div>
             </th>
 
-            <td className="px-6 py-4">11/07</td>
+            <td className="px-6 py-4 font-inter text-base">11/07</td>
             <td className="px-6 py-4">
-              <span className="inline-flex items-center gap-1 rounded-full bg-green-50 px-2 py-1 text-xs font-semibold text-green-800">
-                <span className="h-1.5 w-1.5 rounded-full bg-green-800"></span>
-                Active
+              <span className="inline-flex items-center gap-1 rounded-full bg-green-50 px-2 py-1 text-xs font-semibold text-green-800 border-6 border-green-800">
+                Available
               </span>
             </td>
             <td className="px-6 py-4">
@@ -54,6 +55,11 @@ export default function Home() {
                 <FontAwesomeIcon icon={faStar} className="text-gray-900" />
                 <FontAwesomeIcon icon={faStar} className="text-gray-900" />
                 <FontAwesomeIcon icon={faStar} className="text-gray-900" />
+              </div>
+            </td>
+            <td className="px-6 py-4">
+              <div className="flex items-center justify-center gap-2 h-full">
+                <FontAwesomeIcon icon={faEllipsisV} className="text-gray-800" />
               </div>
             </td>
           </tr>
