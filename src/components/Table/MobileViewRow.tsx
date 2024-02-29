b9c9327ac32fff8e47c8095982604ed767323cd1
helenapaixao <hp.helenapaixao@gmail.com>
2024-02-29 16:51:17 -0300

    [feat] Adjust icon position in mobile View

--- src/components/Table/MobileViewRow.tsx
+++ src/components/Table/MobileViewRow.tsx
@@ -1,7 +1,10 @@
-// MobileViewRow.tsx
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEllipsisV, faCheck, faStar } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEllipsisV,
+  faCheck,
+  faStar,
+} from "@fortawesome/free-solid-svg-icons";
 
 interface MobileViewRowProps {
   data: {
@@ -20,7 +23,7 @@ const MobileViewRow: React.FC<MobileViewRowProps> = ({
   handleImageClick,
 }) => (
   <div className="p-4 flex items-start">
-    <div className="relative h-14 w-14 flex-shrink-0">
+    <div className="relative h-20 w-20 flex-shrink-0">
       {isImageClicked && (
         <div className="absolute top-0 left-0 h-6 w-6 flex items-center justify-center bg-green-600 rounded-full">
           <FontAwesomeIcon icon={faCheck} className="text-white" />
@@ -35,24 +38,23 @@ const MobileViewRow: React.FC<MobileViewRowProps> = ({
     </div>
     <div className="ml-2 flex-grow">
       <div className="text-sm flex items-center">
-        <div className="font-inter font-semibold text-gray-900 text-base">
+        <div className="font-inter font-semibold text-gray-900 text-lg">
           {data.carName}
         </div>
-        <div className="flex items-center justify-center">
-        <FontAwesomeIcon icon={faEllipsisV} className="text-gray-800" />
+        <div className="flex items-center justify-center ml-auto">
+          <FontAwesomeIcon icon={faEllipsisV} className="text-gray-800" />
+        </div>
       </div>
-      </div>
-      <div className="text-gray-500 text-base font-inter font-regular mt-2">
+      <div className="text-gray-500 font-inter font-normal text-sm mt-1">
         Next reservation: {data.nextReservation}
       </div>
-      <div className="mb-2">
+      <div className="mb-2 mt-3">
         <div className="flex gap-2">
           {Array.from({ length: data.rating }, (_, i) => (
             <FontAwesomeIcon key={i} icon={faStar} className="text-gray-900" />
           ))}
         </div>
       </div>
-      
     </div>
   </div>
 );
